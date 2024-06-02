ab0c211bf73d0de29b6197e066adeab5a97c37ee
chenhaoxiang-jcu <haoxiang.chen@my.jcu.edu.au>
2024-06-02 18:26:06 +0800

    Add random score part.

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,8 +1,14 @@
+import random
+
+
 def main():
-    """Ask the user for their score and print the result."""
+    """Ask the user for their score and print the result, then get a random score and print the result."""
     score = float(input("Enter score: "))
     message = determine_message(score)
     print(message)
+    score = random.randint(0, 100)
+    message = determine_message(score)
+    print(f"{score} is considered {message}")
 
 
 def determine_message(score):
