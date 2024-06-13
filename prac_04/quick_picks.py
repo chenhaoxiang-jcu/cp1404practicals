8584b71b82b0fe7482a2ab8b1b761624a6f5ba03
chenhaoxiang-jcu <haoxiang.chen@my.jcu.edu.au>
2024-06-13 18:38:01 +0800

    Modify quick picks program with extracting function.

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -2,16 +2,27 @@ import random
 
 MIN_NUMBER = 1
 MAX_NUMBER = 45
-COLUMN = 6
+NUMBERS_PER_PICK = 6
 
-number_of_picks = int(input("How many quick picks? "))
-for i in range(number_of_picks):
-    quick_picks = []
-    for j in range(COLUMN):
+
+def main():
+    """Print a number of lines randomly selected numbers in ascending order"""
+    number_of_picks = int(input("How many quick picks? "))
+    for i in range(number_of_picks):
+        random_numbers = pick_numbers()
+        [print(f'{number:2}', end=' ') for number in sorted(random_numbers)]
+        print()
+
+
+def pick_numbers():
+    """Pick random numbers without repeated and store them in a list"""
+    random_numbers = []
+    for j in range(NUMBERS_PER_PICK):
         number = random.randint(MIN_NUMBER, MAX_NUMBER)
-        while number in quick_picks:
+        while number in random_numbers:
             number = random.randint(MIN_NUMBER, MAX_NUMBER)
-        quick_picks.append(number)
-    quick_picks.sort()
-    [print(f'{number:2}', end=' ') for number in quick_picks]
-    print()
+        random_numbers.append(number)
+    return random_numbers
+
+
+main()
