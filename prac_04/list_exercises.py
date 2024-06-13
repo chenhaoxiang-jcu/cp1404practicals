f185e4a223f145fd0f8462ac9be681b6978ec116
chenhaoxiang-jcu <haoxiang.chen@my.jcu.edu.au>
2024-06-13 11:33:24 +0800

    Add another list practice.

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -15,3 +15,12 @@ print(f"The first number is {first_number}\n"
       f"The smallest number is {smallest_number}\n"
       f"The largest number is {largest_number}\n"
       f"The average of the numbers is {average}")
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input("Username: ")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
