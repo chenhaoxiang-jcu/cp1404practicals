ee0758ec2eb3605897ea04a8efd9eee841f7a281
chenhaoxiang-jcu <haoxiang.chen@my.jcu.edu.au>
2024-06-15 21:10:00 +0800

    Add upper method onto user's input and a loop that prints all the states and names.

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -8,10 +8,15 @@ CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern T
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
 
-state_code = input("Enter short state: ")
+state_codes = []
+state_code = input("Enter short state: ").upper()
 while state_code != "":
     if state_code in CODE_TO_NAME:
         print(state_code, "is", CODE_TO_NAME[state_code])
+        state_codes.append(state_code)
     else:
         print("Invalid short state")
-    state_code = input("Enter short state: ")
+    state_code = input("Enter short state: ").upper()
+
+abbreviation_width = max(len(state_code) for state_code in state_codes)
+[print(f"{state_code:{abbreviation_width}} is {CODE_TO_NAME[state_code]}") for state_code in state_codes]
