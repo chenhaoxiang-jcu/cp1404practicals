7b86928a90ddd5c696d2e922a15369666fc65bc7
chenhaoxiang-jcu <haoxiang.chen@my.jcu.edu.au>
2024-06-22 17:37:13 +0800

    Modify word occurrences program.

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -7,16 +7,17 @@ Actual:   9 minutes
 
 def main():
     """Get a string user inputted and count the occurrences of words in a string."""
-    text = input("Text: ").split()
-    word_to_count = count_word(text)
-    word_width = max(len(word) for word in text)
+    text = input("Text: ")
+    words = text.split()
+    word_to_count = count_word(words)
+    word_width = max(len(word) for word in words)
     display_word_and_count(word_to_count, word_width)
 
 
-def count_word(text):
+def count_word(words):
     """Count the occurrences of words in a string and store them in a dictionary."""
     word_to_count = {}
-    for word in sorted(text):
+    for word in sorted(words):
         word_to_count[word] = word_to_count.get(word, 0) + 1
     return word_to_count
 
