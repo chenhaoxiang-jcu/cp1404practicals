21b4150811f6e98b574a92c6a1365830d11b7eed
chenhaoxiang-jcu <haoxiang.chen@my.jcu.edu.au>
2024-07-12 20:43:50 +0800

    Modify used_cars program by adding name to previous object which are my_car and limo.

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -9,15 +9,16 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    my_car = Car('Car', 180)
     my_car.drive(30)
     print(f"Car has fuel: {my_car.fuel}")
     print(my_car)
 
-    limo = Car(100)
+    limo = Car('Limo', 100)
     limo.add_fuel(20)
     print(limo.fuel)
     limo.drive(115)
+    print(limo)
 
     harry_car = Car('Nimbus-2000', 2000)
     print(harry_car)
