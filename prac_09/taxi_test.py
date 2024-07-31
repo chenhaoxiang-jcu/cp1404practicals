ff0792d3059271b7846d5b0c2a6bc42d594cbfdf
chenhaoxiang-jcu <haoxiang.chen@my.jcu.edu.au>
2024-07-31 21:56:48 +0800

    Finish class variables practice.

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -1,7 +1,7 @@
 from prac_09.taxi import Taxi
 
 # Create a new taxi object, my_taxi, with name "Prius 1", 100 units of fuel and price of $1.23
-my_taxi = Taxi('Prius 1', 100, 1.23)
+my_taxi = Taxi('Prius 1', 100)
 # Drive the taxi 40 km
 my_taxi.drive(40)
 # Print the taxi's details and the current fare
